ca6167d42d217a4ef9cef013aeb5ec9cd64000de
anjuonline <109638952+anjuonline@users.noreply.github.com>
2022-07-21 21:08:13 -0400

    feature branch test

--- src/pages/Artwork.js
+++ src/pages/Artwork.js
@@ -2,9 +2,33 @@ const Artwork = () => {
   return (
     <>
       <div class="main-column">
+
         <div class="cardContainerPurple">
               <div class="titleWrapPurple">
                   <h2>Hellscape Digital / Artwork</h2>
+              </div>            
+              <div class="containedText">
+                  <h3>
+                      By Artist
+                  </h3>                
+              </div>
+              <div class="flexColumn">
+                    <div class="containedText">
+                        <p>
+                            ANJU Online!<br/>
+                        </p>
+                    </div>
+                    <div class="containedText">
+                        <p>
+                            Cloudi Daze<br/>
+                        </p>
+                    </div>
+                </div>
+          </div>
+
+        <div class="cardContainerBlue">
+              <div class="titleWrapBlue">
+                  <h2>ANJU Online!</h2>
               </div>            
               <div class="containedText">
                   <h3>
