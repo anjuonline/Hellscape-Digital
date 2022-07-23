0478776720e72b26187dc728a1762ba5a21a745e
anjuonline <109638952+anjuonline@users.noreply.github.com>
2022-07-23 17:37:05 -0400

    content updates (music)

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -58,28 +58,75 @@ const Music = () => {
 
         <div class="card-container">
             <div class="title-wrap-rainbow2">
-                    <h2>Music / Latest Releases</h2>
+                <h2>Music / Latest Releases</h2>
+            </div>
+            <div class="gallery_containerThree"> 
+                <div class="gallery_item">
+                    <p class="gallery-tag">Feature</p>
+                    <a href="https://soundcloud.com/tryfg/sodagrape" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-wZfZQLtYA2RTwxXE-gSGKLQ-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>TRYFG</strong><br/>
+                            soda grape! (Feat. Kawaii Ry)<br/>
+                            (Prod. TRYFG)
+                        </p>
+                    </a>
+                </div>            
+                <div class="gallery_item">
+                    <p class="gallery-tag">Single</p>
+                    <a href="https://soundcloud.com/liljosen/done-died-w-jackie-platinum" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-rLd37yeBxMh3DJZz-TW35LQ-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>Josen</strong><br/>
+                            DONE!DIED! (Feat. Jackie Platinum)<br/>
+                            (Prod. Notamachine + Antar + OBMUS1C)
+                        </p>
+                    </a>
                 </div>
-                <div class="gallery_containerThree">
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Single</p><img src="https://i1.sndcdn.com/artworks-NHMUEUaj0ew3bunu-6aFVbw-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Kawaii Ry</strong><br/>KILLMILORD Freestyle<br/>(Prod. Foreigner2x)</p>
-                    </div>    
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Single</p><img src="https://i1.sndcdn.com/artworks-rEyTQxOLQoxWYufg-ip6Zug-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Josen</strong><br/>BITCH!YOU!A!DUB!<br/>(Prod. Snake)</p>
-                    </div> 
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Album</p><img src="https://i1.sndcdn.com/artworks-m07hEzNRsO0mhwyA-V6cMUg-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Notamachine</strong><br/>Artificial</p>
-                    </div>   
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Single</p><img src="https://i1.sndcdn.com/artworks-NHMUEUaj0ew3bunu-6aFVbw-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Kawaii Ry</strong><br/>KILLMILORD Freestyle<br/>(Prod. Foreigner2x)</p>
-                    </div>    
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Single</p><img src="https://i1.sndcdn.com/artworks-rEyTQxOLQoxWYufg-ip6Zug-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Josen</strong><br/>BITCH!YOU!A!DUB!<br/>(Prod. Snake)</p>
-                    </div> 
-                    <div class="gallery_item">
-                        <p class="gallery-tag">Album</p><img src="https://i1.sndcdn.com/artworks-m07hEzNRsO0mhwyA-V6cMUg-t500x500.jpg" width="100%"/><br/><p class="gallery-text"><strong>Notamachine</strong><br/>Artificial</p>
-                    </div>                                         
+                <div class="gallery_item">
+                    <p class="gallery-tag">Single</p>
+                    <a href="https://soundcloud.com/kawaiiry/crash-it-2-prod-wound" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-AFS7gLNHCDzsrluL-GL5WLw-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>Kawaii Ry</strong><br/>
+                            Crash It 2<br/>
+                            (Prod. Wound)
+                        </p>
+                    </a>
+                </div> 
+                <div class="gallery_item">
+                    <p class="gallery-tag">Single</p>
+                    <a href="https://soundcloud.com/kawaiiry/killmilord-freestyle-prod-foriegner2x" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-NHMUEUaj0ew3bunu-6aFVbw-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>Kawaii Ry</strong><br/>
+                            KILLMILORD Freestyle<br/>
+                            (Prod. Foreigner2x)
+                        </p>
+                    </a>
+                </div>
+                <div class="gallery_item">
+                    <p class="gallery-tag">Single</p>
+                    <a href="https://soundcloud.com/kawaiiry/killmilord-freestyle-prod-foriegner2x" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-rEyTQxOLQoxWYufg-ip6Zug-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>Josen</strong><br/>
+                            BITCH!YOU!A!DUB!<br/>
+                            (Prod. Snake)
+                        </p>
+                    </a>
                 </div>  
+                <div class="gallery_item">
+                    <p class="gallery-tag">Album</p>
+                    <a href="https://soundcloud.com/liljosen/final-josen-prod-snake-mixed" target="_blank">    
+                        <img src="https://i1.sndcdn.com/artworks-m07hEzNRsO0mhwyA-V6cMUg-t500x500.jpg" width="100%"/><br/>
+                        <p class="gallery-text">
+                            <strong>Notamachine</strong><br/>
+                            Artificial
+                        </p>
+                    </a>
+                </div>                                                                                                             
+            </div>   
         </div>
 
         <div class="card-container">
@@ -98,6 +145,17 @@ const Music = () => {
                     <h2 id="kawaii">Music / Kawaii Ry</h2>
                 </div>
                 <div class="gallery_containerThree">
+                    <div class="gallery_item">
+                        <p class="gallery-tag">Feature</p>
+                        <a href="https://soundcloud.com/tryfg/sodagrape" target="_blank">    
+                            <img src="https://i1.sndcdn.com/artworks-wZfZQLtYA2RTwxXE-gSGKLQ-t500x500.jpg" width="100%"/><br/>
+                            <p class="gallery-text">
+                                <strong>TRYFG</strong><br/>
+                                soda grape! (Feat. Kawaii Ry)<br/>
+                                (Prod. TRYFG)
+                            </p>
+                        </a>
+                    </div>                     
                     <div class="gallery_item">
                         <p class="gallery-tag">Single</p>
                         <a href="https://soundcloud.com/kawaiiry/crash-it-2-prod-wound" target="_blank">    
