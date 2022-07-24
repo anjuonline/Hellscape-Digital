abd01c300015ac33d9cd04f20189330fb740dee9
anjuonline <109638952+anjuonline@users.noreply.github.com>
2022-07-24 19:45:08 -0400

    archive updates

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -141,6 +141,44 @@ const Music = () => {
         </div>
 
         <div class="card-container">
+            <div class="title-wrap-rainbow2">
+                <h2>Music / Hellscape Radio</h2>
+            </div>            
+            <div class="gallery_containerThree">
+                    <div class="gallery_item">
+                        <p class="gallery-tag">Mix</p>
+                        <a href="https://soundcloud.com/hellscapedigital/hellscape-radio-vol-3" target="_blank">    
+                            <img src="https://i1.sndcdn.com/artworks-oRXsFL7pajM9zxnU-yDhLaA-t500x500.jpg" width="100%"/><br/>
+                            <p class="gallery-text">
+                                <strong>Hellscape Radio, Vol. 3</strong><br/>
+                                (Hosted by ANJU Online!)
+                            </p>
+                        </a>
+                    </div>                     
+                    <div class="gallery_item">
+                        <p class="gallery-tag">Mix</p>
+                        <a href="https://soundcloud.com/hellscapedigital/hellscape-radio-vol-2" target="_blank">    
+                            <img src="https://i1.sndcdn.com/artworks-tF08flAkDpvmjgof-uvDVsg-t500x500.jpg" width="100%"/><br/>
+                            <p class="gallery-text">
+                                <strong>Hellscape Radio, Vol. 2</strong><br/>
+                                (Hosted by ANJU Online!)
+                            </p>
+                        </a>
+                    </div> 
+                    <div class="gallery_item">
+                        <p class="gallery-tag">Mix</p>
+                        <a href="https://soundcloud.com/hellscapedigital/hellscape-radio-vol-1-private-server-underground-minimix-hosted-by-anju-online-1" target="_blank">    
+                            <img src="https://i1.sndcdn.com/artworks-000594396492-7c85fj-t500x500.jpg" width="100%"/><br/>
+                            <p class="gallery-text">
+                                <strong>Hellscape Radio, Vol. 1</strong><br/>
+                                (Hosted by ANJU Online!)
+                            </p>
+                        </a>
+                    </div>                                           
+            </div>
+        </div>        
+
+        <div class="card-container">
             <div class="title-wrap-rainbow">
                     <h2 id="anju">Music / ANJU Online!</h2>
                 </div>
